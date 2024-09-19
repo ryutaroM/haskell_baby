9b2d09ff4d7184473d87a4cfdcca97a98c71a261
ryutaroM <arcticmonkey326@gmail.com>
2024-09-20 00:14:49 +0900

    done up to 5.3

--- baby.hs
+++ baby.hs
@@ -223,3 +223,19 @@ quicksort''' (x : xs) =
   let smallerOrEqual = filter (<= x) xs
       larger = filter (> x) xs
    in quicksort''' smallerOrEqual ++ [x] ++ quicksort''' larger
+
+largetstDivisible :: Integer
+largetstDivisible = head (filter p [100000, 99999 ..])
+  where
+    p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1 .. 100]))
+  where
+    isLong xs = length xs > 15
