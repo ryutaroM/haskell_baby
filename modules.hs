178f8c0e9b95fff45bbd92844cd179014b9b3a88
ryutaroM <arcticmonkey326@gmail.com>
2024-10-26 00:43:40 +0900

    done up to findKey

--- modules.hs
+++ modules.hs
@@ -16,3 +16,21 @@ encode offset msg = map (\c -> C.chr $ C.ord c + offset) msg
 
 decode :: Int -> String -> String
 decode shifter msg = encode (negate shifter) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map C.digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1 ..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1 ..]
+
+findKey :: (Eq k) => k -> [(k, v)] -> v
+findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key [] = Nothing
+findKey' key ((k, v) : xs)
+  | key == k = Just v
+  | otherwise = findKey' key xs
