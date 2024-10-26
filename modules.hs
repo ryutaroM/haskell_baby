c56c7e87a1aa6aa094b7b592e76f170d46bd9cec
ryutaroM <arcticmonkey326@gmail.com>
2024-10-26 12:32:39 +0900

    done up to chap6

--- modules.hs
+++ modules.hs
@@ -34,3 +34,38 @@ findKey' key [] = Nothing
 findKey' key ((k, v) : xs)
   | key == k = Just v
   | otherwise = findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
+
+phoneBook :: M.Map String String
+phoneBook =
+  M.fromList $
+    [ ("betty", "555-2938"),
+      ("bonnie", "452-2928"),
+      ("patsy", "493-2928"),
+      ("lucille", "205-2928"),
+      ("wendy", "939-8282"),
+      ("penny", "853-2492")
+    ]
+
+phoneBook' =
+  [ ("betty", "555-2938"),
+    ("bonnie", "452-2928"),
+    ("patsy", "493-2928"),
+    ("lucille", "205-2928"),
+    ("wendy", "939-8282"),
+    ("penny", "853-2492"),
+    ("betty", "112-321")
+  ]
+
+string2digits :: String -> [Int]
+string2digits = map C.digitToInt . filter C.isDigit
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> M.Map k String
+phoneBookToMap xs = M.fromListWith add xs
+  where
+    add number1 number2 = number1 ++ ", " ++ number2
+
+phoneBookToMap' :: (Ord k) => [(k, a)] -> M.Map k [a]
+phoneBookToMap' xs = M.fromListWith (++) $ map (\(k, v) -> (k, [v])) xs
