5bd45bfd499126b9dc131e79e6447f24b7a33cfe
ryutaroM <arcticmonkey326@gmail.com>
2024-12-08 14:04:22 +0900

    done up to chap11

---
+++ applicativeioaction.hs
@@ -0,0 +1,4 @@
+main = do
+  a <- (++) <$> getLine <*> getLine
+  putStrLn $ "The two lines concatenated turn out to be: " ++ a
+
--- baby.hs
+++ baby.hs
@@ -284,3 +284,18 @@ oddSquareSum = sum (takeWhile (< 10000) (filter odd (map (^ 2) [1 ..])))
 
 oddSquareSum' :: Integer
 oddSquareSum' = sum . takeWhile (< 10000) . filter odd $ map (^ 2) [1 ..]
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter + 1) (f x)
+
+myAction :: IO String
+myAction = do
+  a <- getLine
+  b <- getLine
+  return $ a ++ b
+
+myAction' :: IO String
+myAction' = (++) <$> getLine <*> getLine
