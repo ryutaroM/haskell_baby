2f4de82cf80433836082f11c9015fa1d3594c40b
ryutaroM <arcticmonkey326@gmail.com>
2024-12-15 21:10:19 +0900

    done up to chap14

--- baby.hs
+++ baby.hs
@@ -1,3 +1,9 @@
+import Control.Monad
+import Control.Monad.State
+import Control.Monad.Writer qualified as W
+import Data.Monoid
+import Data.Ratio
+
 doubleMe x = x + x
 
 doubleUs x y = x * 2 + y * 2
@@ -346,3 +352,271 @@ landRight n (left, right)
 
 banana :: Pole -> Maybe Pole
 banana _ = Nothing
+
+foo :: Maybe String
+foo = do
+  x <- Just 3
+  y <- Just "!"
+  Just (show x ++ y)
+
+marySue :: Maybe Bool
+marySue = do
+  x <- Just 9
+  Just (x > 8)
+
+routine :: Maybe Pole
+routine = do
+  start <- return (0, 0)
+  first <- landLeft 2 start
+  second <- landRight 2 first
+  landLeft 1 second
+
+routine' :: Maybe Pole
+routine' = do
+  start <- return (0, 0)
+  first <- landLeft 2 start
+  Nothing
+  second <- landRight 2 first
+  landLeft 1 second
+
+swap :: (a, b) -> (b, a)
+swap = do
+  let f = \(a, b) -> (b, a)
+  f
+
+listOfTuples :: [(Int, Char)]
+listOfTuples = do
+  n <- [1, 2]
+  ch <- ['a', 'b']
+  return (n, ch)
+
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1 .. 50]
+  guard ('7' `elem` show x)
+  return x
+
+type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) =
+  do
+    (c', r') <- [(c + 2, r - 1), (c + 2, r + 1), (c - 2, r - 1), (c - 2, r + 1), (c + 1, r - 2), (c + 1, r + 2), (c - 1, r - 2), (c - 1, r + 2)]
+    guard (c' `elem` [1 .. 8] && r' `elem` [1 .. 8])
+    return (c', r')
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x > 9, "Compared gang size to 9")
+
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, log) f =
+  let (y, newLog) = f x
+   in (y, log ++ newLog)
+
+applyLog' :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog' (x, log) f =
+  let (y, newLog) = f x
+   in (y, log `mappend` newLog)
+
+type Food = String
+
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
+
+logNumber :: Int -> W.Writer [String] Int
+logNumber x = W.writer (x, ["Got number: " ++ show x])
+
+multWithLog :: W.Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  W.tell ["Gonna multiply these two"]
+  return (a * b)
+
+gcd' :: Int -> Int -> Int
+gcd' a b
+  | b == 0 = a
+  | otherwise = gcd' b (a `mod` b)
+
+gcd'' :: Int -> Int -> W.Writer [String] Int
+gcd'' a b
+  | b == 0 = do
+      W.tell ["Finished with " ++ show a]
+      return a
+  | otherwise = do
+      W.tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      gcd'' b (a `mod` b)
+
+newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
+
+gcdReverse :: Int -> Int -> W.Writer [String] Int
+gcdReverse a b
+  | b == 0 = do
+      W.tell ["Finished with " ++ show a]
+      return a
+  | otherwise = do
+      result <- gcdReverse b (a `mod` b)
+      W.tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      return result
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs ++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+  (DiffList f) <> (DiffList g) = DiffList (\xs -> f (g xs))
+
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  mappend = (<>)
+
+gcd''' :: Int -> Int -> W.Writer (DiffList String) Int
+gcd''' a b
+  | b == 0 = do
+      W.tell (toDiffList ["Finished with " ++ show a])
+      return a
+  | otherwise = do
+      W.tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+      gcd''' b (a `mod` b)
+
+finalCountDown :: Int -> W.Writer (DiffList String) ()
+finalCountDown 0 = do
+  W.tell (toDiffList ["0"])
+finalCountDown x = do
+  finalCountDown (x - 1)
+  W.tell (toDiffList [show x])
+
+type Stack = [Int]
+
+pop :: Stack -> (Int, Stack)
+pop (x : xs) = (x, xs)
+
+push :: Int -> Stack -> ((), Stack)
+push a xs = ((), a : xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack =
+  let ((), newStack1) = push 3 stack
+      (a, newStack2) = pop newStack1
+   in pop newStack2
+
+pop' :: State Stack Int
+pop' = state $ \(x : xs) -> (x, xs)
+
+push' :: Int -> State Stack ()
+push' a = state $ \xs -> ((), a : xs)
+
+stackManip' :: State Stack Int
+stackManip' = do
+  push' 3
+  a <- pop'
+  pop'
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop'
+  if a == 5
+    then push' 5
+    else do
+      push' 3
+      push' 8
+
+moreStack :: State Stack ()
+moreStack = do
+  a <- stackManip'
+  if a == 100
+    then stackStuff
+    else return ()
+
+stackyStack :: State Stack ()
+stackyStack = do
+  stackNow <- get
+  if stackNow == [1, 2, 3]
+    then put [8, 3, 1]
+    else put [9, 2, 1]
+
+pop'' :: State Stack Int
+pop'' = do
+  xs <- get
+  case xs of
+    (x : xs') -> do
+      put xs'
+      return x
+    [] -> error "Stack is empty"
+
+push'' :: Int -> State Stack ()
+push'' a = do
+  xs <- get
+  put (a : xs)
+
+binSmalls :: Int -> Int -> Maybe Int
+binSmalls acc x
+  | x > 9 = Nothing
+  | otherwise = Just (acc + x)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of
+  [(x, "")] -> Just x
+  _ -> Nothing
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x : y : ys) "*" = Just ((x * y) : ys)
+foldingFunction (x : y : ys) "+" = Just ((x + y) : ys)
+foldingFunction (x : y : ys) "-" = Just ((y - x) : ys)
+foldingFunction xs numberString = liftM (: xs) (readMaybe numberString)
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+  [result] <- foldM foldingFunction [] (words st)
+  return result
+
+newtype Prob a = Prob {getProb :: [(a, Rational)]} deriving (Show)
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob [(Prob [('a', 1 % 2), ('b', 1 % 2)], 1 % 4), (Prob [('c', 1 % 2), ('d', 1 % 2)], 3 % 4)]
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concatMap multAll xs
+  where
+    multAll (Prob innerxs, p) = map (\(x, r) -> (x, p * r)) innerxs
+
+instance Functor Prob where
+  fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+instance Applicative Prob where
+  pure x = Prob [(x, 1 % 1)]
+  (Prob fs) <*> (Prob xs) = Prob [(f x, p1 * p2) | (f, p1) <- fs, (x, p2) <- xs]
+
+instance Monad Prob where
+  return = pure
+  m >>= f = flatten (fmap f m)
+
+instance MonadFail Prob where
+  fail _ = Prob []
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads, 1 % 2), (Tails, 1 % 2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads, 1 % 10), (Tails, 9 % 10)]
+
+flipThree :: Prob Bool
+flipThree = do
+  a <- coin
+  b <- coin
+  c <- loadedCoin
+  return (all (== Tails) [a, b, c])
